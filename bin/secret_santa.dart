e29116d3efac1b78e640dc9c929aafb70e6dcf79
Monty Rasmussen <montyr75@gmail.com>
2014-01-06 17:52:55 -0700

    better use of final vars

--- bin/secret_santa.dart
+++ bin/secret_santa.dart
@@ -2,9 +2,9 @@
   The program must take a list of people (first name, last name) and return pairs of
   Secret Santas. A person can't be their own santa, and a person can't be a santa for someone
   with the same last name.
-  
+
   Example input:
-  
+
   Zoe Washburne
   Hoban Washburne
   Malcolm Reynolds
@@ -14,7 +14,7 @@
   Dawn Summers
 **/
 
-List<Person> people = [
+final List<Person> people = [
   new Person("Zoe", "Washburne"),
   new Person("Hoban", "Washburne"),
   new Person("Malcolm", "Reynolds"),
@@ -24,45 +24,45 @@ List<Person> people = [
   new Person("Dawn", "Summers")
 ];
 
-List<Person> santas = new List<Person>.from(people, growable: false);
- 
+final List<Person> santas = new List<Person>.from(people, growable: false);
+
 void main() {
   int iterations = 0;
-  
+
   // loop until there are no more invalid pairings
   while (people.any((Person person) => !person.validSanta)) {
     // randomize the santas list
     santas.shuffle();
-    
+
     // map randomized santas list to the people list
     int i = 0;
     people.forEach((person) => people[i].secretSanta = santas[i++]);
-    
+
     // count iterations required to achieve a valid list (for fun)
     iterations++;
   }
-  
+
   // print results
   print("Iterations required: $iterations\n");
   people.forEach((Person person) => print("${person.secretSanta} gives to $person."));
 }
- 
+
 class Person {
   String firstName;
   String lastName;
   Person secretSanta;
- 
+
   Person(this.firstName, this.lastName);
-  
+
   Person.fromMap(Map<String, String> map) {
    firstName =  map["firstName"];
    lastName = map["lastName"];
   }
- 
+
   // Secret Santa rules enforced here
   bool get validSanta => secretSanta != null && !identical(secretSanta, this) && secretSanta.lastName != lastName;
- 
+
   Map<String, String> toMap() => {"firstName": firstName, "lastName": lastName};
-  
+
   @override String toString() => "$firstName $lastName";
 }
